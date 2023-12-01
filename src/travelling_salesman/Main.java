b4600f00b50cb5381bb604e415ba2d00d9552579
dereck lhw <dereck.lamhonwah@gmail.com>
2023-12-01 13:21:51 +0400

    refac: current code implementation into seperate function

--- src/travelling_salesman/Main.java
+++ src/travelling_salesman/Main.java
@@ -18,68 +18,21 @@ public class Main {
         if (cities.isEmpty()) {
             System.out.println("No cities found in the file.");
             return;
-        } else {
-            Scanner scanner = new Scanner(System.in);
-            int choice = 0;
-            boolean validChoice = false;
-
-            while (!validChoice) {
-                System.out.println("Select an algorithm to solve the TSP:");
-                System.out.println("1. Nearest Neighbour");
-                System.out.println("2. Dijkstra's Algorithm");
-                System.out.println("3. Minimum Spanning Tree");
-                System.out.println("4. Exit");
-                System.out.println("");
-
-                // Add more algorithms if you have them
-                System.out.print("Enter your choice (number): ");
-
-                try {
-                    choice = scanner.nextInt();
-
-                    // Check if the choice is within the valid range
-                    if (choice == 1 || choice == 2 || choice == 3 || choice == 4) {
-                        validChoice = true;
-                    } else {
-                        System.out.println("Invalid choice. Please try again.");
-                    }
-                } catch (InputMismatchException e) {
-                    System.out.println("Invalid input. Please enter a number.");
-                    scanner.nextLine(); // Clear the buffer
-                }
-            }
-
-            ArrayList<City> solution = new ArrayList<>();
-            switch (choice) {
-                case 1:
-                    System.out.println("Solving with Algorithm 1...");
-                    solution = solve(cities);
-                    break;
-                case 2:
-                    System.out.println("Solving with Algorithm 2...");
-                    solution = solve(cities);
-                    break;
-                case 3:
-                    System.out.println("Solving with Algorithm 3...");
-                    solution = solve(cities);
-                    break;
-                case 4:
-                    System.out.println("Exiting...");
-                    System.exit(0);
-                    break;
-                default:
-                    break;
-            }
-
-            for (int i = 0; i < solution.size(); i++) {
-                if (i > 0) {
-                    System.out.print("-");
-                }
-                System.out.print(solution.get(i).getNumber());
-            }
-            System.out.println();
-            scanner.close();	
         }
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            int choice = getUserChoice(scanner);
+
+            if (choice == 4) {
+                System.out.println("Exiting...");
+                return; // natural termination
+            }
+
+            System.out.println("Solving with Algorithm " + choice + "...");
+            ArrayList<City> solution = solve(cities);
+            displaySolution(solution);
+        }
+
     }
 
     /**
@@ -93,11 +46,12 @@ public class Main {
         try {
             File file = new File(filePath);
             Scanner scanner = new Scanner(file);
-    
+
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine().trim(); // Trim each line
-                if (line.isEmpty()) continue; // Skip empty lines
-    
+                if (line.isEmpty())
+                    continue; // Skip empty lines
+
                 String[] parts = line.split("\\s+");
                 if (parts.length == 3) {
                     try {
@@ -105,7 +59,7 @@ public class Main {
                         double x = Double.parseDouble(parts[1]);
                         double y = Double.parseDouble(parts[2]);
                         cities.add(new City(cityNumber, x, y));
-                        // System.out.println("Read city: " + cityNumber + " (" + x + ", " + y + ")"); // Optional: For debugging
+                        // System.out.println("Read city: " + cityNumber + " (" + x + ", " + y + ")");
                     } catch (NumberFormatException e) {
                         System.out.println("Invalid number format in line: " + line + " - " + e.getMessage());
                     }
@@ -113,19 +67,58 @@ public class Main {
                     System.out.println("Invalid line format: " + line);
                 }
             }
-    
+
             scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + filePath);
         }
-    
+
         return cities;
     }
-      // Nearest Neighbour Algorithm for TSP
-      public static ArrayList<City> solve(ArrayList<City> cities) {
+
+    private static int getUserChoice(Scanner scanner) {
+        int choice = 0;
+        boolean validChoice = false;
+
+        while (!validChoice) {
+            displayMenu();
+            try {
+                choice = scanner.nextInt();
+                validChoice = choice >= 1 && choice <= 4;
+                if (!validChoice) {
+                    System.out.println("Invalid choice. Please try again.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.nextLine(); // clear the buffer
+            }
+        }
+        return choice;
+    }
+
+    private static void displayMenu() {
+        System.out.println("Select an algorithm to solve the TSP:");
+        System.out.println("1. Nearest Neighbour");
+        System.out.println("2. Dijkstra's Algorithm");
+        System.out.println("3. Minimum Spanning Tree");
+        System.out.println("4. Exit");
+        System.out.print("Enter your choice (number): ");
+    }
+
+    private static void displaySolution(ArrayList<City> solution) {
+        for (int i = 0; i < solution.size(); i++) {
+            if (i > 0)
+                System.out.print("-");
+            System.out.print(solution.get(i).getNumber());
+        }
+        System.out.println();
+    }
+
+    // Nearest Neighbour Algorithm for TSP
+    public static ArrayList<City> solve(ArrayList<City> cities) {
         ArrayList<City> tour = new ArrayList<>();
         ArrayList<City> remainingCities = new ArrayList<>(cities);
-        
+
         // Start from the first city
         City currentCity = remainingCities.remove(0);
         tour.add(currentCity);
@@ -137,14 +130,14 @@ public class Main {
             remainingCities.remove(nearestCity);
             currentCity = nearestCity;
         }
-        
+
         // Return to the starting city
         tour.add(tour.get(0));
         return tour;
     }
 
-     // Method to find the nearest city to the current city
-     private static City findNearestCity(City currentCity, ArrayList<City> cities) {
+    // Method to find the nearest city to the current city
+    private static City findNearestCity(City currentCity, ArrayList<City> cities) {
         City nearestCity = null;
         double minDistance = Double.MAX_VALUE;
         for (City city : cities) {
@@ -157,5 +150,3 @@ public class Main {
         return nearestCity;
     }
 }
-
-
