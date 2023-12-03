1af458fe0a5bab12fd013615c07ebdac5569ac6f
dereck lhw <dereck.lamhonwah@gmail.com>
2023-12-03 23:39:19 +0400

    feat: implement MST

--- src/travelling_salesman/Solver.java
+++ src/travelling_salesman/Solver.java
@@ -3,8 +3,10 @@ package travelling_salesman;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class Solver {
     public static ArrayList<City> solveNearestNeighbour(ArrayList<City> cities) {
@@ -106,10 +108,6 @@ public class Solver {
         return shortestPathMap.get(endCity);
     }
 
-    public static ArrayList<City> solveMST(ArrayList<City> cities) {
-        return cities;
-    }
-
     // Helper class to pair cities with their distances
     private static class CityDistancePair {
         private City city;
@@ -126,6 +124,83 @@ public class Solver {
 
         public double getDistance() {
             return distance;
+        }
+    }
+
+    public static ArrayList<City> solveMST(ArrayList<City> cities) {
+        // Create a graph from the cities
+        int n = cities.size();
+        double[][] graph = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                graph[i][j] = cities.get(i).distanceTo(cities.get(j));
+            }
+        }
+
+        // Prim's algorithm to find MST
+        boolean[] visited = new boolean[n];
+        double[] key = new double[n];
+        int[] parent = new int[n];
+        PriorityQueue<CityEdge> pq = new PriorityQueue<>(Comparator.comparingDouble(CityEdge::getWeight));
+
+        for (int i = 0; i < n; i++) {
+            key[i] = Double.MAX_VALUE;
+            parent[i] = -1;
+        }
+        key[0] = 0;
+        pq.offer(new CityEdge(0, 0));
+
+        while (!pq.isEmpty()) {
+            int u = pq.poll().getDest();
+            visited[u] = true;
+            for (int v = 0; v < n; v++) {
+                if (!visited[v] && graph[u][v] < key[v]) {
+                    key[v] = graph[u][v];
+                    pq.offer(new CityEdge(v, key[v]));
+                    parent[v] = u;
+                }
+            }
+        }
+
+        // Depth-First Search (DFS) traversal of the MST to create the tour
+        ArrayList<City> tour = new ArrayList<>();
+        Set<Integer> visitedCities = new HashSet<>();
+        dfsMST(0, parent, visitedCities, cities, tour);
+
+        tour.add(tour.get(0)); // Add the start city at the end to complete the tour
+        return tour;
+    }
+
+    private static void dfsMST(int current, int[] parent, Set<Integer> visitedCities, ArrayList<City> cities,
+            ArrayList<City> tour) {
+        visitedCities.add(current);
+        tour.add(cities.get(current));
+
+        for (int i = 0; i < parent.length; i++) {
+            if (parent[i] == current && !visitedCities.contains(i)) {
+                dfsMST(i, parent, visitedCities, cities, tour);
+            }
+        }
+    }
+
+    // Helper class for edges in the graph
+    private static class CityEdge {
+        private int dest;
+        private double weight;
+
+        public CityEdge(int dest, double weight) {
+            this.dest = dest;
+            this.weight = weight;
+        }
+
+        public int getDest() {
+            return dest;
+        }
+
+        public double getWeight() {
+            return weight;
         }
     }
 }
