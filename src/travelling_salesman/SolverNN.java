ed64405f8302ac1ee8211f6fba2b9728393eeb06
dereck lhw <dereck.lamhonwah@gmail.com>
2023-12-05 21:00:12 +0400

    refac: change the visited cities to a hashset to improve performance in bigger dataset

--- src/travelling_salesman/SolverNN.java
+++ src/travelling_salesman/SolverNN.java
@@ -1,62 +1,51 @@
 package travelling_salesman;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
- * Utility class for solving the Travelling Salesman Problem.
+ * Utility class for solving the Travelling Salesman Problem using the Nearest
+ * Neighbour algorithm.
  */
 public class SolverNN {
     /**
-     * Solves the Travelling Salesman Problem using the Nearest Neighbour
-     * algorithm.
-     * 
+     * Solves the Travelling Salesman Problem using the Nearest Neighbour algorithm.
+     *
      * @param cities The list of cities to use in the algorithm.
      * @return The solution to the Travelling Salesman Problem.
      */
     public static ArrayList<City> solveNearestNeighbour(ArrayList<City> cities) {
-
         ArrayList<City> tour = new ArrayList<>();
-        ArrayList<City> remainingCities = new ArrayList<>(cities);
+        HashSet<Integer> visitedCities = new HashSet<>();
 
-        // Start from the first city and remove it from the list of remaining cities
-        City currentCity = remainingCities.remove(0);
+        // Start from the first city
+        City currentCity = cities.get(0);
         tour.add(currentCity);
+        visitedCities.add(currentCity.getNumber());
 
         // Iterate until all cities are visited
-        while (!remainingCities.isEmpty()) {
-            // Find the nearest city from the current city
-            City nearestCity = findNearestCityNearestNeighbour(currentCity, remainingCities);
+        while (tour.size() < cities.size()) {
+            City nearestCity = null;
+            double minDistance = Double.MAX_VALUE;
+
+            // Find the nearest unvisited city
+            for (City city : cities) {
+                if (!visitedCities.contains(city.getNumber())) {
+                    double distance = currentCity.distanceTo(city);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        nearestCity = city;
+                    }
+                }
+            }
+
             tour.add(nearestCity);
-            remainingCities.remove(nearestCity);
+            visitedCities.add(nearestCity.getNumber());
             currentCity = nearestCity;
         }
 
+        // Return to the starting city to complete the tour
         tour.add(tour.get(0));
         return tour;
     }
-
-    /**
-     * Finds the nearest city to the current city
-     * 
-     * @param currentCity The current city.
-     * @param cities      The list of cities to search.
-     * 
-     * @return The nearest city to the current city.
-     */
-    private static City findNearestCityNearestNeighbour(City currentCity, ArrayList<City> cities) {
-        City nearestCity = null;
-        double minDistance = Double.MAX_VALUE;
-
-        // Iterate through each city in the list
-        for (City city : cities) {
-            double distance = currentCity.distanceTo(city);
-            // Check if the calculated distance is less than the minimum distance found
-            if (distance < minDistance) {
-                minDistance = distance;
-                nearestCity = city;
-            }
-        }
-        return nearestCity;
-    }
-
 }
